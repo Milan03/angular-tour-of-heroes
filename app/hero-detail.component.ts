793fed82a89af906475688fe55152d4b1abea8f3
Sobat <Milan.Sobat@sykes.com>
2017-01-17 15:38:37 -0500

    added API services

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -17,16 +17,20 @@ export class HeroDetailComponent implements OnInit {
     @Input()
     hero: Hero;
     constructor(
-        private heroSerivce: HeroService,
+        private heroService: HeroService,
         private route: ActivatedRoute,
         private location: Location
     ) {}
     ngOnInit(): void {
         this.route.params
-            .switchMap((params: Params) => this.heroSerivce.getHero(+params['id']))
+            .switchMap((params: Params) => this.heroService.getHero(+params['id']))
             .subscribe(hero => this.hero = hero);
     }
     goBack(): void {
         this.location.back();
     }
+    save(): void {
+        this.heroService.update(this.hero)
+                        .then(() => this.goBack());
+}
 }
