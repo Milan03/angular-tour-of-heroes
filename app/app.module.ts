a88e7d2862311918238722844041d77c4442e0c6
Sobat <Milan.Sobat@sykes.com>
2017-01-17 09:16:45 -0500

    Heroes component HTML CSS file decoupling

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,37 +1,21 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { FormsModule }   from '@angular/forms';
+import { RouterModule }  from '@angular/router';
 
-import { AppComponent }  from './app.component';
-import { HeroesComponent } from './heroes.component';
+import { AppComponent }        from './app.component';
+import { HeroesComponent }     from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
-import { HeroService } from './hero.service';
-import { DashboardComponent } from './dashboard.component';
+import { HeroService }         from './hero.service';
+import { DashboardComponent }  from './dashboard.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [ 
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: 'detail/:id',
-        component: HeroDetailComponent
-      },
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      }
-    ])
+    AppRoutingModule
   ],
   declarations: [ 
     AppComponent,
@@ -39,9 +23,7 @@ import { DashboardComponent } from './dashboard.component';
     HeroDetailComponent,
     HeroesComponent
   ],
-  providers: [
-    HeroService
-  ],
+  providers: [ HeroService ],
   bootstrap:[ AppComponent ]
 })
 export class AppModule { }
